c097a9b98df780b5ee69dc0210b87eaf7b6bba53
edersonlrf <edersonluis.rf@gmail.com>
2019-04-19 17:09:09 -0300

    1.4 IMPLEMENTAÇÃO DA INTERAÇÃO DA ACTION INDEX COM A VISÃO

--- SIstemaEnsinoSuperior/Cap01/Controllers/InstituicaoController.cs
+++ SIstemaEnsinoSuperior/Cap01/Controllers/InstituicaoController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cap01.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cap01.Controllers
 {
     public class InstituicaoController : Controller
     {
+        private static IList<Instituicao> instituicoes =
+            new List<Instituicao>()
+                {
+                    new Instituicao() {
+                        InstituicaoID = 1,
+                        Nome = "UniParaná",
+                        Endereco = "Paraná"
+                    },
+                    new Instituicao() {
+                        InstituicaoID = 2,
+                        Nome = "UniSanta",
+                        Endereco = "Santa Catarina"
+                    },
+                    new Instituicao() {
+                        InstituicaoID = 3,
+                        Nome = "UniSãoPaulo",
+                        Endereco = "São Paulo"
+                    },
+                    new Instituicao() {
+                        InstituicaoID = 4,
+                        Nome = "UniSulgrandense",
+                        Endereco = "Rio Grande do Sul"
+                    },
+                    new Instituicao() {
+                        InstituicaoID = 5,
+                        Nome = "UniCarioca",
+                        Endereco = "Rio de Janeiro"
+                    }
+                };
+
         public IActionResult Index()
         {
-            return View();
+            return View(instituicoes);
         }
     }
 }
