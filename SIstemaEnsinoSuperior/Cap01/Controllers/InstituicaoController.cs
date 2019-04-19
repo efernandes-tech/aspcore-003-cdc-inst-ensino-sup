93c2f1e2acaae8dd4e2fd3762de59b0505dc4d42
edersonlrf <edersonluis.rf@gmail.com>
2019-04-19 16:48:34 -0300

    1.2 CRIANDO O CONTROLADOR PARA INSTITUIÇÕES DE ENSINO

---
+++ SIstemaEnsinoSuperior/Cap01/Controllers/InstituicaoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cap01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
