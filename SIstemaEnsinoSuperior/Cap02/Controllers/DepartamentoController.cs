93d58c6cb2773f1a7c79a67c5b18832b2cd57935
edersonlrf <edersonluis.rf@gmail.com>
2019-04-20 18:13:56 -0300

    2.3 Visualização dos dados existentes pela action/view Index

--- SIstemaEnsinoSuperior/Cap02/Controllers/DepartamentoController.cs
+++ SIstemaEnsinoSuperior/Cap02/Controllers/DepartamentoController.cs
@@ -11,6 +11,16 @@ namespace Cap02.Controllers
 {
     public class DepartamentoController : Controller
     {
+        private readonly IESContext _context;
 
+        public DepartamentoController(IESContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.OrderBy(c => c.Nome).ToListAsync());
+        }
     }
 }
