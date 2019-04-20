ae48ba36127a6408fd22ea4a590a96dd6fb099cc
edersonlrf <edersonluis.rf@gmail.com>
2019-04-20 18:12:14 -0300

    2.3 IMPLEMENTAÇÃO DO CRUD FAZENDO USO DO ENTITY FRAMEWORK CORE

---
+++ SIstemaEnsinoSuperior/Cap02/Controllers/DepartamentoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cap02.Data;
+using Cap02.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cap02.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+
+    }
+}
