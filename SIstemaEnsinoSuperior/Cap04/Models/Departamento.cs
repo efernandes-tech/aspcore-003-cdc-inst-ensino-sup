c9bffb8acb5c5713fcadff18db448b3da910a2a6
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-17 11:14:01 -0300

    CAPÍTULO 4 - ASSOCIAÇÕES NO ENTITY FRAMEWORK CORE

---
+++ SIstemaEnsinoSuperior/Cap04/Models/Departamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cap04.Models
+{
+    public class Departamento
+    {
+        public long? DepartamentoID { get; set; }
+        public string Nome { get; set; }
+    }
+}
