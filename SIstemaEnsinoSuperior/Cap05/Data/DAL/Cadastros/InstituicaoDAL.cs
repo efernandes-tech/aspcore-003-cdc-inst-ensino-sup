46f83c0206b7f9297746b8a22dc4ba4bd1b02008
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-21 16:40:12 -0300

    5.3 CRIANDO A CAMADA DE PERSISTÊNCIA EM UMA PASTA DA APLICAÇÃO

---
+++ SIstemaEnsinoSuperior/Cap05/Data/DAL/Cadastros/InstituicaoDAL.cs
@@ -0,0 +1,23 @@
+﻿using Modelo.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cap05.Data.DAL.Cadastros
+{
+    public class InstituicaoDAL
+    {
+        private IESContext _context;
+
+        public InstituicaoDAL(IESContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Instituicao> ObterInstituicaoClassificadasPorNome()
+        {
+            return _context.Instituicoes.OrderBy(b => b.Nome);
+        }
+    }
+}
