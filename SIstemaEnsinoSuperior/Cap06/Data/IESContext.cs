cf45d85d2063708521f22619e3e1987da6ba2f56
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-21 20:58:44 -0300

    CAPÍTULO 6 - CODE FIRST MIGRATIONS, DATA ANNOTATIONS E VALIDAÇÕES

---
+++ SIstemaEnsinoSuperior/Cap06/Data/IESContext.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Cadastros;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cap06.Data
+{
+    public class IESContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+    }
+}
