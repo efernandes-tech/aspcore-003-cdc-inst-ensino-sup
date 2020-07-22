132094b1a48fcc79a8fb2fa8b1eb8a1b93c0d802
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-22 10:49:43 -0300

    CAPÍTULO 8 - UPLOADS, DOWNLOADS E ERROS

---
+++ SIstemaEnsinoSuperior/Cap08/Areas/Cadastros/Controllers/DepartamentoController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cap08.Data;
+using Modelo.Cadastros;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cap08.Data.DAL.Cadastros;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cap08.Areas.Cadastros.Controllers
+{
+    [Area("Cadastros")]
+    public class DepartamentoController : Controller
+    {
+        private readonly IESContext _context;
+        private readonly DepartamentoDAL departamentoDAL;
+        private readonly InstituicaoDAL instituicaoDAL;
+
+        public DepartamentoController(IESContext context)
+        {
+            this._context = context;
+            instituicaoDAL = new InstituicaoDAL(context);
+            departamentoDAL = new DepartamentoDAL(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await departamentoDAL.ObterDepartamentosClassificadosPorNome().ToListAsync());
+        }
+
+        // GET: Departamento/Create
+        public IActionResult Create()
+        {
+            var instituicoes = instituicaoDAL.ObterInstituicaoClassificadasPorNome().ToList();
+            instituicoes.Insert(0, new Instituicao() { InstituicaoID = 0, Nome = "Selecione a instituição" });
+            ViewBag.Instituicoes = instituicoes;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome, InstituicaoID")] Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await departamentoDAL.GravarDepartamento(departamento);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir os dados.");
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamento/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            ViewResult visaoDepartamento = (ViewResult) await ObterVisaoDepartamentoPorId(id);
+            Departamento departamento = (Departamento)visaoDepartamento.Model;
+            ViewBag.Instituicoes = new SelectList(instituicaoDAL.ObterInstituicaoClassificadasPorNome(), "InstituicaoID", "Nome", departamento.InstituicaoID);
+            return visaoDepartamento;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("DepartamentoID,Nome,InstituicaoID")] Departamento departamento)
+        {
+            if (id != departamento.DepartamentoID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await departamentoDAL.GravarDepartamento(departamento);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if ( ! await DepartamentoExists(departamento.DepartamentoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Instituicoes = new SelectList(instituicaoDAL.ObterInstituicaoClassificadasPorNome(), "InstituicaoID", "Nome", departamento.InstituicaoID);
+            return View(departamento);
+        }
+
+        private async Task<bool> DepartamentoExists(long? id)
+        {
+            return await departamentoDAL.ObterDepartamentoPorId((long) id) != null;
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            return await ObterVisaoDepartamentoPorId(id);
+        }
+
+        // GET: Departamento/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            return await ObterVisaoDepartamentoPorId(id);
+        }
+
+        // POST: Departamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var departamento = await departamentoDAL.EliminarDepartamentoPorId((long) id);
+            TempData["Message"] = "Departamento " + departamento.Nome.ToUpper() + " foi removido";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<IActionResult> ObterVisaoDepartamentoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var departamento = await departamentoDAL.ObterDepartamentoPorId((long)id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+    }
+}
---
+++ SIstemaEnsinoSuperior/Cap08/Areas/Cadastros/Controllers/InstituicaoController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cap08.Data;
+using Modelo.Cadastros;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cap08.Data.DAL.Cadastros;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cap08.Areas.Cadastros.Controllers
+{
+    [Area("Cadastros")]
+    [Authorize]
+    public class InstituicaoController : Controller
+    {
+        private readonly IESContext _context;
+        private readonly InstituicaoDAL instituicaoDAL;
+
+        public InstituicaoController(IESContext context)
+        {
+            this._context = context;
+            instituicaoDAL = new InstituicaoDAL(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await instituicaoDAL.ObterInstituicaoClassificadasPorNome().ToListAsync());
+        }
+
+        // GET: Instituicao/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Endereco")] Instituicao instituicao)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await instituicaoDAL.GravarInstituicao(instituicao);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir os dados.");
+            }
+            return View(instituicao);
+        }
+
+        // GET: Instituicao/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            return await ObterVisaoInstituicaoPorId(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("InstituicaoID,Nome,Endereco")] Instituicao instituicao)
+        {
+            if (id != instituicao.InstituicaoID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await instituicaoDAL.GravarInstituicao(instituicao);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if ( ! await InstituicaoExistsAsync(instituicao.InstituicaoID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instituicao);
+        }
+
+        private async Task<bool> InstituicaoExistsAsync(long? id)
+        {
+            return await instituicaoDAL.ObterInstituicaoPorId((long) id) != null;
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            return await ObterVisaoInstituicaoPorId(id);
+        }
+
+        // GET: Instituicao/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            return await ObterVisaoInstituicaoPorId(id);
+        }
+
+        // POST: Instituicao/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var instituicao = await instituicaoDAL.EliminarInstituicaoPorId((long) id);
+            TempData["Message"] = "Instituição " + instituicao.Nome.ToUpper() + " foi removida";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<IActionResult> ObterVisaoInstituicaoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instituicao = await instituicaoDAL.ObterInstituicaoPorId((long) id);
+            if (instituicao == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituicao);
+        }
+    }
+}
