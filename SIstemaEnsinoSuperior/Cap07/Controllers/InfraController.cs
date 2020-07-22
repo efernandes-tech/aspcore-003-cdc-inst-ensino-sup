ea71241e7606aa3f51d1c03ebdbaac2c90e5f0d1
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-22 10:14:06 -0300

    7.4 REGISTRO DE UM NOVO USUÁRIO

--- SIstemaEnsinoSuperior/Cap07/Controllers/InfraController.cs
+++ SIstemaEnsinoSuperior/Cap07/Controllers/InfraController.cs
@@ -36,5 +36,60 @@ namespace Cap07.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult RegistrarNovoUsuario(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegistrarNovoUsuario(RegistrarNovoUsuarioViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new UsuarioDaAplicacao
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Usuário criou uma nova conta com senha.");
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation("Usuário acesso com a conta criada.");
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
     }
 }
