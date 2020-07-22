9850dcdafad4be558b23cee1dbd18fbab1ffb437
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-22 10:06:04 -0300

    7.3 CRIAÇÃO DE UM ACESSO AUTENTICADO

---
+++ SIstemaEnsinoSuperior/Cap07/Controllers/InfraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cap07.Models.Infra;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Cap07.Controllers
+{
+    [Authorize]
+    public class InfraController : Controller
+    {
+        private readonly UserManager<UsuarioDaAplicacao> _userManager;
+        private readonly SignInManager<UsuarioDaAplicacao> _signInManager;
+        private readonly ILogger _logger;
+
+        public InfraController(
+            UserManager<UsuarioDaAplicacao> userManager,
+            SignInManager<UsuarioDaAplicacao> signInManager,
+            ILogger<InfraController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Acessar(string returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
