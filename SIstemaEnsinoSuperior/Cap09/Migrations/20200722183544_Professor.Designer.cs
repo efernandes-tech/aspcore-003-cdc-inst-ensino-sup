2b07c7197e74948421c304de772e4bc0dbf44c33
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-07-22 15:45:08 -0300

    update

--- SIstemaEnsinoSuperior/Cap09/Migrations/20200722183544_Professor.Designer.cs
+++ SIstemaEnsinoSuperior/Cap09/Migrations/20200722183544_Professor.Designer.cs
@@ -4,14 +4,16 @@ using Cap09.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cap09.Migrations
 {
     [DbContext(typeof(IESContext))]
-    partial class IESContextModelSnapshot : ModelSnapshot
+    [Migration("20200722183544_Professor")]
+    partial class Professor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
